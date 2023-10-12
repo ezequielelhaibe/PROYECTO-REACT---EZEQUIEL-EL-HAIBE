2525f719cbbf4558f790b1f28ac95112942fe3e3
Ezequiel El Haibe <ezequiel.elhaibe@gmail.com>
2023-10-11 21:12:59 -0300

    primercommit

---
+++ elhaibe-proyecto/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './componets/Navbar/Navbar'
+
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar />
+      <h1>PROYECTO AGENCIA</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}</button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ elhaibe-proyecto/src/componets/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar () {
+    return (
+      <nav>
+        <ul>
+          <li>INICIO</li>
+          <li>SECCION 1</li>
+          <li>SECCION 2</li>
+        </ul>
+      </nav>
+    )
+  }
+
+export default Navbar
